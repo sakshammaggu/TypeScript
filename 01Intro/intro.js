5e7f5088f93a02554d0cf6c3ee4b125253e0683a
sakshammaggu <sakshammaggu80@gmail.com>
2024-07-03 01:58:45 +0530

    installation, setup of typescript and intro

---
+++ 01Intro/intro.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+var user = { name: "saksham", age: 10 };
+console.log(user);
+console.log(user.name);
+// console.log(user.email); ---> gives an error
---
+++ 01Intro/intro.ts
@@ -0,0 +1,6 @@
+console.log("Hello World");
+
+const user={name: "saksham", age: 10};
+console.log(user);
+console.log(user.name);
+// console.log(user.email); ---> gives an error
