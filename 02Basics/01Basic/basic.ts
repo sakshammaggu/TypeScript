416acb6781f5855c4c538c4ccdf5d8b348d72b32
sakshammaggu <sakshammaggu80@gmail.com>
2024-07-04 00:36:36 +0530

    basics of ts - syntax

---
+++ 02Basics/01Basic/basic.ts
@@ -0,0 +1,9 @@
+// methods of str are string based only
+let str:string="Hello World";   // syntax of ts 
+console.log(str);
+
+// methods of num are number based only
+let num:number=7;
+console.log(num);
+
+export {};
